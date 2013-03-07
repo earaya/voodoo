d927d94da33d2c6e4474b0a4f239f93d2cf1bafa
Esteban Araya <esteban.araya@gmail.com>
2013-03-06 17:42:52 -0700

    Put Talis copyright notice back to comply w/ Apache 2.0 license.

--- src/test/java/com/earaya/voodoo/apitest/ServerAgentHeaderAndLoggingFilterAcceptanceTest.java
+++ src/test/java/com/earaya/voodoo/apitest/ServerAgentHeaderAndLoggingFilterAcceptanceTest.java
@@ -1,3 +1,18 @@
+/*
+ *    Copyright 2011 Talis Systems Ltd
+ * 
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package com.earaya.voodoo.apitest;
 
 
@@ -51,7 +66,7 @@ public class ServerAgentHeaderAndLoggingFilterAcceptanceTest {
         HttpGet httpGet = new HttpGet(getUrl(httpPort, "foo"));
         HttpResponse response = httpClient.execute(httpGet);
         assertEquals(404, response.getStatusLine().getStatusCode());
-        assertVoodooResponseIdPresent(response);
+        assertTalisResponseIdPresent(response);
         assertServerAgentHeaderPresent(response);
     }
 
@@ -60,7 +75,7 @@ public class ServerAgentHeaderAndLoggingFilterAcceptanceTest {
         HttpGet httpGet = new HttpGet(getUrl(httpPort, "stub"));
         HttpResponse response = httpClient.execute(httpGet);
         assertEquals(200, response.getStatusLine().getStatusCode());
-        assertVoodooResponseIdPresent(response);
+        assertTalisResponseIdPresent(response);
         assertServerAgentHeaderPresent(response);
     }
 
@@ -69,7 +84,7 @@ public class ServerAgentHeaderAndLoggingFilterAcceptanceTest {
         HttpGet httpGet = new HttpGet(getUrl(httpPort, "stub/internalErr"));
         HttpResponse response = httpClient.execute(httpGet);
         assertEquals(500, response.getStatusLine().getStatusCode());
-        assertVoodooResponseIdPresent(response);
+        assertTalisResponseIdPresent(response);
         assertServerAgentHeaderPresent(response);
     }
 
@@ -89,9 +104,9 @@ public class ServerAgentHeaderAndLoggingFilterAcceptanceTest {
         assertEquals(expectedServerAgent, serverHeader.getValue());
     }
 
-    private void assertVoodooResponseIdPresent(HttpResponse response) {
-        Header voodooResponsId = response.getFirstHeader(LoggingFilter.X_VOODOO_RESPONSE_ID);
-        assertNotNull(voodooResponsId);
+    private void assertTalisResponseIdPresent(HttpResponse response) {
+        Header talisResponseId = response.getFirstHeader(LoggingFilter.X_TALIS_RESPONSE_ID);
+        assertNotNull(talisResponseId);
     }
 }
 
