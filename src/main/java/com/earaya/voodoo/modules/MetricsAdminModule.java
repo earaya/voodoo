1538658fcb3303d5127e9da98204804975aa98d5
Esteban Araya <esteban.araya@gmail.com>
2013-03-07 21:13:40 -0700

    Add MetricsAdminModule. I'll probably be replacing this with the Yammer Guice integration package on Github..

---
+++ src/main/java/com/earaya/voodoo/modules/MetricsAdminModule.java
@@ -0,0 +1,15 @@
+package com.earaya.voodoo.modules;
+
+import com.google.inject.servlet.ServletModule;
+import com.yammer.metrics.reporting.AdminServlet;
+
+import javax.inject.Singleton;
+
+public class MetricsAdminModule extends ServletModule {
+
+    @Override
+    protected void configureServlets() {
+        bind(AdminServlet.class).in(Singleton.class);
+        serve("/metrics-admin/*").with(AdminServlet.class);
+    }
+}
