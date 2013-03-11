7071884dbe3ea709d65b52405e0a39e2a2e5635f
Esteban Araya <esteban.araya@gmail.com>
2013-03-11 16:29:05 -0600

    Move VudoResourceConfig bidning out of VuduModule.

--- src/main/java/com/earaya/voodoo/modules/VuduModule.java
+++ src/main/java/com/earaya/voodoo/modules/VuduModule.java
@@ -1,28 +1,18 @@
 package com.earaya.voodoo.modules;
 
-import com.earaya.voodoo.VuduResourceConfig;
 import com.google.inject.AbstractModule;
 import com.google.inject.servlet.ServletModule;
 import com.yammer.metrics.guice.InstrumentationModule;
-import com.yammer.metrics.jersey.InstrumentedResourceMethodDispatchAdapter;
 import com.yammer.metrics.reporting.AdminServlet;
 
 import javax.inject.Singleton;
 
 public class VuduModule extends AbstractModule {
-    private final VuduResourceConfig resourceConfig;
-
-    public VuduModule(VuduResourceConfig resourceConfig) {
-        this.resourceConfig = resourceConfig;
-        this.resourceConfig.getClasses().add(InstrumentedResourceMethodDispatchAdapter.class);
-        // TODO: add CacheControlDispatcher from DropWizard.
-    }
 
     @Override
     protected void configure() {
         install(new InstrumentationModule());
         install(new MetricsAdminModule());
-        bind(VuduResourceConfig.class).toInstance(resourceConfig);
     }
 
     public class MetricsAdminModule extends ServletModule {
