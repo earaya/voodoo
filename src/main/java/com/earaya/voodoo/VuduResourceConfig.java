b11ce0ecc82e7d0f48741e04a4ac6b8b6cc46e1a
Esteban Araya <esteban.araya@gmail.com>
2013-03-11 16:29:39 -0600

    Fix VuduResourceConfig to scan multiple packages.

--- src/main/java/com/earaya/voodoo/VuduResourceConfig.java
+++ src/main/java/com/earaya/voodoo/VuduResourceConfig.java
@@ -1,14 +1,19 @@
 package com.earaya.voodoo;
 
+import com.earaya.voodoo.exceptions.DefaultExceptionMapper;
 import com.sun.jersey.api.core.PackagesResourceConfig;
+import com.yammer.metrics.jersey.InstrumentedResourceMethodDispatchAdapter;
 
 public class VuduResourceConfig extends PackagesResourceConfig {
 
-    public VuduResourceConfig() {
-        super("com.earaya.voodoo");
+    public VuduResourceConfig(String... packageName) {
+        super(packageName);
+        this.getClasses().add(InstrumentedResourceMethodDispatchAdapter.class);
+        this.getSingletons().add(new ObjectMapperProvider());
+        this.getSingletons().add(new DefaultExceptionMapper());
     }
 
-    public void addSingleton(Object provdier) {
-        this.getSingletons().add(provdier);
+    public void addProvider(Object provider) {
+        this.getSingletons().add(provider);
     }
 }
