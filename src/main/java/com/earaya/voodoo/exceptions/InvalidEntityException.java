93a7dedfcfb36fb5f41f0e4e18764cfe54c7af71
Scott Brown <scott.brown@domo.com>
2013-03-21 08:44:28 -0600

    Change the InvalidEntityException to be a HTTPException that returns a BAD_REQUEST (400) when the entity could not be validated.

--- src/main/java/com/earaya/voodoo/exceptions/InvalidEntityException.java
+++ src/main/java/com/earaya/voodoo/exceptions/InvalidEntityException.java
@@ -2,13 +2,15 @@ package com.earaya.voodoo.exceptions;
 
 import com.google.common.collect.ImmutableList;
 
-public class InvalidEntityException extends RuntimeException {
+import javax.ws.rs.core.Response;
+
+public class InvalidEntityException extends HttpException {
     private static final long serialVersionUID = -8762073181655035705L;
 
     private final ImmutableList<String> errors;
 
     public InvalidEntityException(String message, Iterable<String> errors) {
-        super(message);
+        super(Response.Status.BAD_REQUEST, message);
         this.errors = ImmutableList.copyOf(errors);
     }
 
