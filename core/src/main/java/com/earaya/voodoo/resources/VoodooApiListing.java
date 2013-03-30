ef79095966a639b6d7033812a1ef344ec488a640
Scott Brown <scott.brown@domo.com>
2013-03-30 07:53:23 -0600

    Missing VoodooApiListing

---
+++ core/src/main/java/com/earaya/voodoo/resources/VoodooApiListing.java
@@ -0,0 +1,7 @@
+package com.earaya.voodoo.resources;
+
+@javax.ws.rs.Path("/docs")
+@com.wordnik.swagger.annotations.Api("/docs")
+@javax.ws.rs.Produces({"application/json"})
+public class VoodooApiListing extends com.wordnik.swagger.jaxrs.listing.ApiListing {
+}
